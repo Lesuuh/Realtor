09dbc7de976ee100e20a247cb44a69c1dc7bf95e
Lesuuh <lesuuh01@gmail.com>
2024-03-17 03:37:59 +0100

    created and completed the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Offers } from "./pages/Offers";
+import { Profile } from "./pages/Profile";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { ForgetPassword } from "./pages/ForgetPassword";
+import { Header } from "./components/Header";
 
 function App() {
-
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Header />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <div></div>
+      <div>
+        <RouterProvider router={router} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,124 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
+import {
+  MdCloseFullscreen,
+  MdOutlineAccountCircle,
+  MdOutlineMenu,
+} from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { useState } from "react";
+
+export const Header = () => {
+  const [menu, setMenu] = useState(false);
+  const location = useLocation();
+
+  // function to check the pathname and return true
+  const RoutePathName = (routes) => {
+    if (routes === location.pathname) {
+      return true;
+    }
+  };
+
+  const handleMenu = () => {
+    setMenu(!menu);
+    console.log(menu);
+  };
+  return (
+    <>
+      <div className={`bg-white border-b shadow-sm sticky top-0 `}>
+        <div className="flex h-[60px]  items-center justify-between px-3 sm:px-16 max-w-6xl mx-auto ">
+          <div onClick={() => handleMenu()} className={`sm:hidden pr-2`}>
+            {menu ? (
+              <span>
+                <MdCloseFullscreen size={25} />
+              </span>
+            ) : (
+              <span>
+                <MdOutlineMenu size={25} />
+              </span>
+            )}
+          </div>
+          <div className="mr-auto">
+            <Link to="/">
+              <h2 className="text-red-500 text-xl font-semibold ">
+                Real<span className="text-black">tors</span>
+              </h2>
+            </Link>
+          </div>
+
+          {/* full screen */}
+          <nav className="flex items-center">
+            <ul className={`hidden sm:flex gap-5`}>
+              <li
+                className={`hover:underline hover:underline-offset-8 hover:text-red-500 text-sm font-semibold ${
+                  RoutePathName("/") && "font-bold text-red-500"
+                }`}
+              >
+                <Link to="/" onClick={() => setMenu(false)}>
+                  Home
+                </Link>
+              </li>
+
+              <li
+                className={`hover:underline hover:underline-offset-8 hover:text-red-500 text-sm font-semibold ${
+                  RoutePathName("/offers") && "font-bold text-red-500"
+                }`}
+              >
+                <Link to="/offers" onClick={() => setMenu(false)}>
+                  Offers
+                </Link>
+              </li>
+
+              <li
+                className={`hover:underline hover:underline-offset-8 hover:text-red-500 text-sm font-semibold ${
+                  RoutePathName("/signin") && "font-bold text-red-500"
+                }`}
+              >
+                <Link to="/signin" onClick={() => setMenu(false)}>
+                  Sign in
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* mobile screen */}
+          <nav className="">
+            <ul
+              className={`flex sm:hidden gap-5 text-sm px-5 py-5 ${
+                menu
+                  ? "flex left-0  justify-start items-start "
+                  : "-left-[100%] ease-in "
+              } absolute top-[60px] duration-300 ease-in  bg-gray-900 text-white h-[100dvh] w-[70%] flex-col  `}
+            >
+              <li className="hover:underline hover:underline-offset-8 hover:text-red-500">
+                <Link
+                  to="/"
+                  onClick={() => setMenu(false)}
+                  className="flex gap-1 items-center justify-center"
+                >
+                  <FaHome /> Home
+                </Link>
+              </li>
+
+              <li className="hover:underline hover:underline-offset-8 hover:text-red-500">
+                <Link to="/offers" onClick={() => setMenu(false)}>
+                  Offers
+                </Link>
+              </li>
+              <li className="hover:underline hover:underline-offset-8 hover:text-red-500">
+                <Link to="/signin" onClick={() => setMenu(false)}>
+                  Sign in
+                </Link>
+              </li>
+            </ul>
+            <Link to="/profile" className="sm:hidden">
+              <MdOutlineAccountCircle size={25} />
+            </Link>
+          </nav>
+        </div>
+      </div>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
